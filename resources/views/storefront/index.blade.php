35eb283020aeb831ccbd8fe93b36f3223b1d1e62
Peter Kimutai <peterkimutai004@gmail.com>
2026-08-25 10:10:55 +0300

    commit

--- resources/views/storefront/index.blade.php
+++ resources/views/storefront/index.blade.php
@@ -74,7 +74,7 @@
 
     {{-- Product image - fills the entire circle --}}
     <div class="absolute inset-2 rounded-full overflow-hidden border-4 border-white/20 shadow-2xl">
-        <img src="{{ asset('Medium (Front View).png') }}" alt="PINACARE Eco Baby Care" class="w-full h-full object-cover">
+        <img src="{{ asset('Medium (Front view).png') }}" alt="PINACARE Eco Baby Care" class="w-full h-full object-cover">
 
     </div>
 
